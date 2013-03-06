cebb1d4a33fe62b74c7e49453acd534793174939
Ben Postlethwaite <post.ben.here@gmail.com>
2013-03-05 21:44:33 -0800

    Begin testing

---
+++ test/test.js
@@ -0,0 +1,21 @@
+var cmap = require('./..')
+  , test = require("tape")
+
+
+test('is object - object', function(t) {
+  t.plan(1)
+  var n = 15
+  , cg
+  , check = true
+
+  // Display all the colormaps
+  var cms = ['jet', 'hsv' ,'hot', 'cool', 'spring', 'summer', 'autumn',
+           'winter', 'gray', 'bone', 'copper']
+
+  for (var i = 0; i < cms.length; i++) {
+    cg = cmap({'colormap': cms[6], 'nshades': n })
+    check = check & (cg.length == n)
+  }
+
+  t.ok(check)
+})
