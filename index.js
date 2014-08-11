3146efa50816fae19e5fb4422e1d813da2db0137
ben postlethwaite <post.ben.here@gmail.com>
2014-08-11 09:55:19 -0400

    upgrade and expand colormaps

--- index.js
+++ index.js
@@ -3,268 +3,84 @@
  * January 2013
  * License MIT
  */
-var at = require('arraytools')
+'use strict';
+var at = require('arraytools');
+var colorScale = require('./colorScales.json');
+
 
 module.exports = function (spec) {
 
   /*
    * Default Options
    */
-   if( !at.isObj(spec) )
-    spec = {}
-  spec.colormap = spec.colormap || "jet"
-  spec.nshades = spec.nshades || 72
-  spec.format = spec.format || "hex"
+    var indicies, fromrgb, torgb,
+        nsteps, cmap,
+        nshades, colors,
+        r = [],
+        g = [],
+        b = [];
 
 
-  /*
-   * Supported colormaps
-   */
-   var cmaps = {
-    jet: {
-      r: [
-      [0.000, 0.376, 0.627, 0.878, 1.000]
-      , [0.000, 0.016, 1.000, 0.984, 0.500]
-      ]
-      , g: [
-      [0.000, 0.125, 0.376, 0.627, 0.878, 1.000]
-      , [0.000, 0.016, 1.000, 0.984, 0.000, 0.000]
-      ]
-      , b: [
-      [0.000, 0.125, 0.376, 0.627, 1.000]
-      , [0.516, 1.000, 0.984, 0.000, 0.000]
-      ]
+    if ( !at.isPlainObject(spec) ) spec = {};
 
+    spec.colormap = spec.colormap || 'jet';
+    spec.nshades = nshades = spec.nshades || 72;
+    spec.format = spec.format || 'hex';
+
+    if (!(spec.colormap in colorScale)) {
+        throw Error(spec.colormap + ' not a supported colorscale');
     }
-    , hsv: {
-      r: [
-      [0.000, 0.169, 0.173, 0.337, 0.341, 0.671, 0.675, 0.839, 0.843, 1.000]
-      , [1.000, 0.992, 0.969, 0.000, 0.000, 0.008, 0.031, 1.000, 1.000, 1.000]
-      ]
-      , g: [
-      [0.000, 0.169, 0.173, 0.506, 0.671, 0.675, 1.000]
-      , [0.000, 1.000, 1.000, 0.977, 0.000, 0.000, 0.000]
-      ]
-      , b: [
-      [0.000, 0.337, 0.341, 0.506, 0.839, 0.843, 1.000]
-      , [0.000, 0.016, 0.039, 1.000, 0.984, 0.961, 0.023]
-      ]
 
-    }
-    , hot: {
-      r: [
-      [0.000, 0.376, 1.000]
-      , [0.010, 1.000, 1.000]
-      ]
-      , g: [
-      [0.000, 0.376, 0.753, 1.000]
-      , [0.000, 0.010, 1.000, 1.000]
-      ]
-      , b: [
-      [0.000, 0.753, 1.000]
-      , [0.000, 0.016, 1.000]
-      ]
+    cmap = colorScale[spec.colormap];
 
-    }
-    , cool: {
-      r: [
-      [0.000, 1.000]
-      , [0.000, 1.000]
-      ]
-      , g: [
-      [0.000, 1.000]
-      , [1.000, 0.000]
-      ]
-      , b: [
-      [0.000, 1.000]
-      , [1.000, 1.000]
-      ]
-
-    }
-    , spring: {
-      r: [
-      [0.000, 1.000]
-      , [1.000, 1.000]
-      ]
-      , g: [
-      [0.000, 1.000]
-      , [0.000, 1.000]
-      ]
-      , b: [
-      [0.000, 1.000]
-      , [1.000, 0.000]
-      ]
-
-    }
-    , summer: {
-      r: [
-      [0.000, 1.000]
-      , [0.000, 1.000]
-      ]
-      , g: [
-      [0.000, 1.000]
-      , [0.500, 1.000]
-      ]
-      , b: [
-      [0.000, 1.000]
-      , [0.400, 0.400]
-      ]
-
-    }
-    , autumn: {
-      r: [
-      [0.000, 1.000]
-      , [1.000, 1.000]
-      ]
-      , g: [
-      [0.000, 1.000]
-      , [0.000, 1.000]
-      ]
-      , b: [
-      [0.000, 1.000]
-      , [0.000, 0.000]
-      ]
-
-    }
-    , winter: {
-      r: [
-      [0.000, 1.000]
-      , [0.000, 0.000]
-      ]
-      , g: [
-      [0.000, 1.000]
-      , [0.000, 1.000]
-      ]
-      , b: [
-      [0.000, 1.000]
-      , [1.000, 0.500]
-      ]
-
-    }
-    , gray: {
-      r: [
-      [0.000, 1.000]
-      , [0.000, 1.000]
-      ]
-      , g: [
-      [0.000, 1.000]
-      , [0.000, 1.000]
-      ]
-      , b: [
-      [0.000, 1.000]
-      , [0.000, 1.000]
-      ]
-
-    }
-    , bone: {
-      r: [
-      [0.000, 0.753, 1.000]
-      , [0.000, 0.661, 1.000]
-      ]
-      , g: [
-      [0.000, 0.376, 0.753, 1.000]
-      , [0.000, 0.331, 0.784, 1.000]
-      ]
-      , b: [
-      [0.000, 0.376, 1.000]
-      , [0.001, 0.454, 1.000]
-      ]
-
-    }
-    , copper: {
-      r: [
-      [0.000, 0.804, 1.000]
-      , [0.000, 1.000, 1.000]
-      ]
-      , g: [
-      [0.000, 1.000]
-      , [0.000, 0.781]
-      ]
-      , b: [
-      [0.000, 1.000]
-      , [0.000, 0.497]
-      ]
-
-    }
-  }
-
-  /*
-   * apply map and convert result if needed
-   */
-  var carray = buildmap(cmaps[spec.colormap], spec.nshades)
-  var result = []
-  if (spec.format === "hex") {
-    carray.forEach( function (ar) {
-      result.push( rgb2hex(ar) )
-    })
-  } else result = carray
-
-
-
-  /*
-   * colormap function
-   *
-   */
-  function buildmap(cmap, n) {
-
-    var div, val, res = []
-    var key = ['r', 'g', 'b']
-
-    for (var i = 0; i < 3; i++) {
-      /*
-       * Check inputs
-       */
-      if (cmap[key[i]][0].length > n) {
+    if (cmap.length > nshades) {
         throw new Error(spec.colormap +
-                        ' map requires nshades to be at least size ' + cmap[key[i]][0].length)
-      }
-
-      /*
-       * map x axis point from 0->1 to 0 -> n-1
-       */
-      div = cmap[key[i]][0].map(function(x) { return x * n }).map( Math.round )
-      /*
-       * map 0 -> 1 rgb value to 0 -> 255
-       */
-      val = cmap[key[i]][1].map(function(x) { return x * 255 })
-
-      /*
-       * Build linear values from x axis point to x axis point
-       * and from rgb value to value
-       */
-      res[i] = lines( div, val ).map( Math.round )
+                        ' map requires nshades to be at least size ' +
+                        cmap.length);
     }
+
     /*
-     * Then zip up 3xn vectors into nx3 vectors
+     * map index points from 0->1 to 0 -> n-1
      */
+    indicies = cmap.map(function(c) {
+        return Math.round(c.index * nshades);
+    });
 
-    return at.zip3(res[0], res[1], res[2])
-  }
 
-  /*
-   * RGB2HEX
-   */
-  function rgb2hex(rgbarray) {
-    var hex = '#'
-    rgbarray.forEach( function (dig) {
-      dig = dig.toString(16)
-      hex += ("00" + dig).substr( dig.length )
-    })
-    return hex
-  }
-
-  function lines (x , y) {
     /*
-     * Inputs are vector x and y, where x defines the ranges
-     * to
+     * map increasing linear values between indicies to
+     * linear steps in colorvalues
      */
-    var a = []
-    for (var i = 0; i < x.length - 1; i++)
-      a = a.concat( at.linspace(y[i], y[i+1], x[i+1] - x[i] ) )
-    return a
-  }
+    for (var i = 0; i < indicies.length-1; ++i) {
+        nsteps = indicies[i+1] - indicies[i];
+        fromrgb = cmap[i].rgb;
+        torgb = cmap[i+1].rgb;
+        r = r.concat(at.linspace(fromrgb[0], torgb[0], nsteps ) );
+        g = g.concat(at.linspace(fromrgb[1], torgb[1], nsteps ) );
+        b = b.concat(at.linspace(fromrgb[2], torgb[2], nsteps ) );
+    }
 
-   return result
+    r = r.map( Math.round );
+    g = g.map( Math.round );
+    b = b.map( Math.round );
+
+    colors = at.zip3(r, g, b);
+
+    if (spec.format === 'hex') colors = colors.map( rgb2hex );
+
+    return colors;
+};
 
 
+/*
+ * RGB2HEX
+ */
+function rgb2hex(rgb) {
+    var dig, hex = '#';
+    for (var i = 0; i < 3; ++i) {
+        dig = rgb[i];
+        dig = dig.toString(16);
+        hex += ('00' + dig).substr( dig.length );
+    }
+    return hex;
 }
